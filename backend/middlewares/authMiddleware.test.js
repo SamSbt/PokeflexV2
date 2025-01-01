0b05a8ae6278a169fd728fbd6b126003d7b48397
Sam Sabat <samnsabat@gmail.com>
2025-01-01 22:55:49 +0100

    minor fix

---
+++ backend/middlewares/authMiddleware.test.js
@@ -0,0 +1,7 @@
+// installation à la racine npm install --save-dev jest
+// fichiers de config à la racine : back, et front pour plus tard
+// scripts et type ajoutés dans package.json à la racine
+// npm install dotenv --save-dev (obligatoire pour les tests)
+// création fichier test dans middlewares folder
+// npm run test:backend ou npm run test:frontend
+// écriture des tests authenticate et hasRole
