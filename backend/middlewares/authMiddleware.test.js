47b07f84fb5043ac10c243af6690b658d22ff242
Sam Sabat <samnsabat@gmail.com>
2025-01-02 18:23:08 +0100

    minor fix

--- backend/middlewares/authMiddleware.test.js
+++ backend/middlewares/authMiddleware.test.js
@@ -1,11 +1,3 @@
-// installation à la racine npm install --save-dev jest
-// fichiers de config à la racine : back, et front pour plus tard
-// scripts et type ajoutés dans package.json à la racine
-// npm install dotenv --save-dev (obligatoire pour les tests)
-// création fichier autMiddleware.test dans middlewares folder
-// npm run test:backend ou npm run test:frontend
-// écriture des tests authenticate et hasRole
-
 import { jest } from "@jest/globals";
 import mongoose from "mongoose";
 
@@ -13,7 +5,6 @@ import mongoose from "mongoose";
 // Mock de verifyAccessToken
 jest.mock("../utils/jwtUtils.js", () => ({
 	verifyAccessToken: jest.fn(),
-
 }));
 import { authenticate, hasRole } from "../middlewares/authMiddleware.js";
 import * as jwtUtils from "../utils/jwtUtils.js";
@@ -89,7 +80,6 @@ beforeEach(() => {
 	it("renvoie 401 si le refresh token est invalide", async () => {
 		req.header.mockReturnValue(undefined);
 		req.cookies.jwt = "invalid_refresh_token";
-
 		fetch.mockResolvedValueOnce({ ok: false });
 
 		await authenticate(req, res, next);
@@ -123,26 +113,26 @@ describe("hasRole Middleware", () => {
 	let req, res, next;
 
 	beforeEach(() => {
-		req = { user: null }; // Simuler une requête avec ou sans utilisateur
+		req = { user: null }; 
 		res = {
 			status: jest.fn().mockReturnThis(),
 			json: jest.fn(),
-		}; // Simuler la réponse
-		next = jest.fn(); // Mock pour next()
+		}; 
+		next = jest.fn(); 
 	});
 
 	it("autorise l'accès si l'utilisateur a le rôle requis", () => {
-		req.user = { role_name: "Dresseur" }; // Simuler un utilisateur avec le rôle "Dresseur"
+		req.user = { role_name: "Dresseur" };
 		const middleware = hasRole("Dresseur");
 
 		middleware(req, res, next);
 
-		expect(next).toHaveBeenCalled(); // Vérifie que next() a été appelé
-		expect(res.status).not.toHaveBeenCalled(); // Vérifie que la réponse n'a pas été modifiée
+		expect(next).toHaveBeenCalled(); 
+		expect(res.status).not.toHaveBeenCalled();
 	});
 
 	it("autorise l'accès si l'utilisateur est Admin", () => {
-		req.user = { role_name: "Admin" }; // Simuler un utilisateur admin
+		req.user = { role_name: "Admin" }; 
 		const middleware = hasRole("Dresseur");
 
 		middleware(req, res, next);
@@ -152,21 +142,21 @@ describe("hasRole Middleware", () => {
 	});
 
 	it("refuse l'accès si l'utilisateur n'a pas le rôle requis", () => {
-		req.user = { role_name: "Visiteur" }; // Simuler un utilisateur avec un rôle incorrect
+		req.user = { role_name: "Visiteur" }; // simuler rôle incorrect
 		const middleware = hasRole("Dresseur");
 
 		middleware(req, res, next);
 
-		expect(res.status).toHaveBeenCalledWith(403); // Vérifie que le statut 403 est retourné
+		expect(res.status).toHaveBeenCalledWith(403);
 		expect(res.json).toHaveBeenCalledWith({
 			success: false,
 			message: "Accès refusé",
 		});
-		expect(next).not.toHaveBeenCalled(); // Vérifie que next() n'a pas été appelé
+		expect(next).not.toHaveBeenCalled(); 
 	});
 
 	it("refuse l'accès si l'utilisateur n'est pas authentifié", () => {
-		req.user = null; // Aucun utilisateur
+		req.user = null; // aucun utilisateur
 		const middleware = hasRole("Dresseur");
 
 		middleware(req, res, next);
@@ -183,3 +173,11 @@ describe("hasRole Middleware", () => {
 afterAll(async () => {
 	await mongoose.connection.close();
 });
+
+// installation à la racine npm install --save-dev jest
+// fichiers de config à la racine : back, et front pour plus tard
+// scripts et type ajoutés dans package.json à la racine
+// npm install dotenv --save-dev (obligatoire pour les tests)
+// création fichier autMiddleware.test dans middlewares folder
+// npm run test:backend ou npm run test:frontend
+// écriture des tests authenticate et hasRole
